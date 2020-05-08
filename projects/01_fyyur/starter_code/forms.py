764d426719c5ca294ffb567a98c6acf882ca6370
Manuela Mercado <mmercado@intellisys.com.do>
2020-05-08 10:57:21 -0400

    Revert "Revert "Fyyur Project" (#4)"
    This reverts commit 71c24710ae65e6e507c940f2cbd6e7deef9ccd28.

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 
 class ShowForm(Form):
@@ -88,33 +88,39 @@ class VenueForm(Form):
     image_link = StringField(
         'image_link'
     )
-    genres = SelectMultipleField(
-        # TODO implement enum restriction
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
-    )
+    genres = [
+        ('Alternative', 'Alternative'),
+        ('Blues', 'Blues'),
+        ('Classical', 'Classical'),
+        ('Country', 'Country'),
+        ('Electronic', 'Electronic'),
+        ('Folk', 'Folk'),
+        ('Funk', 'Funk'),
+        ('Hip-Hop', 'Hip-Hop'),
+        ('Heavy Metal', 'Heavy Metal'),
+        ('Instrumental', 'Instrumental'),
+        ('Jazz', 'Jazz'),
+        ('Musical Theatre', 'Musical Theatre'),
+        ('Pop', 'Pop'),
+        ('Punk', 'Punk'),
+        ('R&B', 'R&B'),
+        ('Reggae', 'Reggae'),
+        ('Rock n Roll', 'Rock n Roll'),
+        ('Soul', 'Soul'),
+        ('Other', 'Other'),
+    ]
+    genres = SelectMultipleField( 'genres', validators=[DataRequired(), AnyOf(values=genres)], choices=genres )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
+    )
+    website = StringField(
+        'website', validators=[URL()]
+    )
+    seeking_talent = BooleanField(
+        'seeking_talent', default=False
+    )
+    seeking_description = StringField(
+        'seeking_description'
     )
 
 class ArtistForm(Form):
@@ -187,34 +193,41 @@ class ArtistForm(Form):
     image_link = StringField(
         'image_link'
     )
-    genres = SelectMultipleField(
-        # TODO implement enum restriction
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
-    )
+    genres = [
+        ('Alternative', 'Alternative'),
+        ('Blues', 'Blues'),
+        ('Classical', 'Classical'),
+        ('Country', 'Country'),
+        ('Electronic', 'Electronic'),
+        ('Folk', 'Folk'),
+        ('Funk', 'Funk'),
+        ('Hip-Hop', 'Hip-Hop'),
+        ('Heavy Metal', 'Heavy Metal'),
+        ('Instrumental', 'Instrumental'),
+        ('Jazz', 'Jazz'),
+        ('Musical Theatre', 'Musical Theatre'),
+        ('Pop', 'Pop'),
+        ('Punk', 'Punk'),
+        ('R&B', 'R&B'),
+        ('Reggae', 'Reggae'),
+        ('Rock n Roll', 'Rock n Roll'),
+        ('Soul', 'Soul'),
+        ('Other', 'Other'),
+    ]
+    genres = SelectMultipleField( 'genres', validators=[DataRequired(), AnyOf(values=genres)], choices=genres )
+
     facebook_link = StringField(
         # TODO implement enum restriction
         'facebook_link', validators=[URL()]
+    )
+    website = StringField(
+        'website', validators=[URL()]
+    )
+    seeking_venue = BooleanField(
+        'seeking_venue', default=False
+    )
+    seeking_description = StringField(
+        'seeking_description'
     )
 
 # TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
