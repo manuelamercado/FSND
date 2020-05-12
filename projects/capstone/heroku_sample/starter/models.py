f99c9e18555f4fa6c4a69bbd7486ba8e581bcb7f
Manuela Mercado <mmercado@intellisys.com.do>
2020-05-12 09:33:19 -0400

    Adding initial conf

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String, create_engine
+import os
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_path = os.environ['DATABASE_URL']
+database_name = "capstone"
+database_path = "postgresql://{}/{}".format(os.environ['DATABASE_URL'], database_name)
 
 db = SQLAlchemy()
 
@@ -17,24 +19,79 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+'''
+Movies and Actors table relantionship
+'''
+movies = db.Table('movies',
+    db.Column('movie_id', Integer, ForeignKey('Movie.id'), primary_key=True),
+    db.Column('actor_id', Integer, ForeignKey('Actor.id'), primary_key=True)
+)
 
 '''
-Person
-Have title and release year
+Actor
+Entity for persons that acts in movies
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class Actor(db.Model):  
+  __tablename__ = 'Actor'
 
   id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+  name = Column(String, nullable=False)
+  age = Column(Integer, nullable=False)
+  gender = Column(String, nullable=False)
+  movies = db.relationship('Movie', secondary=movies, backref=db.backref('movies'))
 
-  def __init__(self, name, catchphrase=""):
+  def __init__(self, name, age, gender, movie=''):
     self.name = name
-    self.catchphrase = catchphrase
+    self.age = age
+    self.gender = gender
+    self.movies = movie
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'catchphrase': self.catchphrase}
+      'age': self.age,
+      'gender': self.gender}
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+'''
+Movie
+Entity with title and release year
+'''
+class Movie(db.Model):  
+  __tablename__ = 'Movie'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String, nullable=False)
+  release_year = Column(DateTime, nullable=False)
+
+  def __init__(self, title, release_year):
+    self.title = title
+    self.release_year = release_year
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_year': self.release_year}
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,1 @@
-
+export DATABASE_URL = 'postgres@localhost:5432'
